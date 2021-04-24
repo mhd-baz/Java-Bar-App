911df1b9c5db39b4d2aa55427a4ef800136b43e2
mhd-baz <76588674+mhd-baz@users.noreply.github.com>
2021-04-25 01:47:13 +0200

    Update and rename run.sh to RENDU FINAL/run.sh

---
+++ RENDU FINAL/run.sh
@@ -0,0 +1,2 @@
+java --module-path "[VOTRE CHEMIN POUR ACCÉDER À JAVAFX]" --add-modules javafx.controls,javafx.fxml -jar PROJET-GMI1-GROUPE_7.jar 
+
--- run.sh
+++
@@ -1,2 +0,0 @@
-java --module-path "/data/Documents/openjfx-16_linux-x64_bin-sdk/javafx-sdk-16/lib" --add-modules javafx.controls,javafx.fxml -jar PROJET-GMI1-GROUPE_7.jar 
-
